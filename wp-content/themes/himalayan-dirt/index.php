d6c86692abd5678e9bc83c609bf22b09054ba7fa
Deepesh Mali <deepeshmali@Deepeshs-MacBook-Air.local>
2019-05-23 16:45:51 +0545

    updates

--- wp-content/themes/himalayan-dirt/index.php
+++ wp-content/themes/himalayan-dirt/index.php
@@ -28,82 +28,163 @@ get_header(); ?>
             <section id="himalayan-dirt-rides">
                 <div class="container">
                     <div class="row">
-                        <div class="col-md-6 aos-item" data-aos="fade-right">
-                            <div class="himalayan-ride-image">
-                                <img src="image/logo.png" alt="Himalayan dirt ride to annapurna base camp">
+                        <div class="col-md-8 aos-item" data-aos="fade-right">
+                            <div class="himalayan-ride-image mt-0">
+                                <img src="<?php echo bloginfo('template_directory') ?>/image/himalayan-dirt-rides-1.png"
+                                     alt="Himalayan dirt ride to annapurna base camp">
                             </div>
                         </div>
-                        <div class="col-md-6 aos-item" data-aos="fade-left">
-                            <div class="himalayan-dirt-discription">
+                        <div class="col-md-4 aos-item" data-aos="fade-left">
+                            <div class="himalayan-dirt-discription pl-5 mt-0">
                                 <span class="featured">Featured</span>
                                 <h2>Mesokanto la via Tilicho Lake</h2>
                                 <p>To provide all sorts of Mountain Biking Tours in the
                                     Himalayas with the extensive knowledge of Terrains and Culture in a professional
                                     manner.
                                     Shred the Himalayan Dirt! with a rider owned Mountain Bike Tour Company.</p>
-                                <a href="#" title="Learn more about tilicho lake">Learn More</a>
+                                <a href="#" title="Learn more about tilicho lake"><span>Learn More</span>
+                                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                         y="0px"
+                                         viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;"
+                                         xml:space="preserve">
+<path style="fill:#1E201D;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
+	C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
+	c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/>
+</svg>
+                                </a>
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-6 aos-item" data-aos="fade-right">
-                            <div class="himalayan-dirt-discription">
+                        <div class="col-md-4 aos-item" data-aos="fade-right">
+                            <div class="himalayan-dirt-discription pr-5">
                                 <span class="featured">Featured</span>
                                 <h2>Mesokanto la via Tilicho Lake</h2>
                                 <p>To provide all sorts of Mountain Biking Tours in the
                                     Himalayas with the extensive knowledge of Terrains and Culture in a professional
                                     manner.
                                     Shred the Himalayan Dirt! with a rider owned Mountain Bike Tour Company.</p>
-                                <a href="#" title="Learn more about tilicho lake">Learn More</a>
+                                <a href="#" title="Learn more about tilicho lake"><span>Learn More</span>
+                                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                         y="0px"
+                                         viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;"
+                                         xml:space="preserve">
+<path style="fill:#1E201D;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
+	C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
+	c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/>
+</svg>
+                                </a>
                             </div>
                         </div>
-                        <div class="col-md-6 aos-item" data-aos="fade-left">
+                        <div class="col-md-8 aos-item" data-aos="fade-left">
                             <div class="himalayan-ride-image">
-                                <img src="image/logo.png" alt="Himalayan dirt ride to annapurna base camp">
+                                <img src="<?php echo bloginfo('template_directory') ?>/image/himalayan-dirt-rides-2.png"
+                                     alt="Himalayan dirt ride to annapurna base camp">
                             </div>
                         </div>
                     </div>
                     <div class="row">
-                        <div class="col-md-6 aos-item" data-aos="fade-right">
+                        <div class="col-md-8 aos-item" data-aos="fade-right">
                             <div class="himalayan-ride-image">
-                                <img src="image/logo.png" alt="Himalayan dirt ride to annapurna base camp">
+                                <img src="<?php echo bloginfo('template_directory') ?>/image/himalayan-dirt-rides-3.png"
+                                     alt="Himalayan dirt ride to annapurna base camp">
                             </div>
                         </div>
-                        <div class="col-md-6 aos-item" data-aos="fade-left">
-                            <div class="himalayan-dirt-discription">
+                        <div class="col-md-4 aos-item" data-aos="fade-left">
+                            <div class="himalayan-dirt-discription pl-5">
                                 <span class="featured">Featured</span>
                                 <h2>Mesokanto la via Tilicho Lake</h2>
                                 <p>To provide all sorts of Mountain Biking Tours in the
                                     Himalayas with the extensive knowledge of Terrains and Culture in a professional
                                     manner.
                                     Shred the Himalayan Dirt! with a rider owned Mountain Bike Tour Company.</p>
-                                <a href="#" title="Learn more about tilicho lake">Learn More</a>
+                                <a href="#" title="Learn more about tilicho lake"><span>Learn More</span>
+                                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                         y="0px"
+                                         viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;"
+                                         xml:space="preserve">
+<path style="fill:#1E201D;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
+	C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
+	c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/>
+</svg>
+                                </a>
                             </div>
                         </div>
                     </div>
                 </div>
             </section>
-            <section class="about-bimal-gurung">
+            <section class="about-bimal-gurung pt-0 pb-0">
                 <div class="row no-gutters">
-                    <div class="col-md-5">
-                        <div class="intro-bimal"></div>
-                    </div>
-                    <div class="col-md-7">
-                        <div class="about-bimal">
+                    <div class="col-md-5" data-aos="fade-down">
+                        <div class="intro-bimal">
+                            <p>
+                                To provide all sorts of Mountain Biking Tours in the Himalayas with the extensive
+                                knowledge of Terrains and Culture in a professional manner.
+                                Shred the Himalayan Dirt!
 
+                            </p>
+                            <p>Nepal was once a forbidden kingdom, hidden from the world, all her mysteries and secrets
+                                kept inside her mystical landlocked boarders. In the 1950’s when the curtain fell and
+                                her borders opened to a rapidly changing world looking for adventure and adrenaline,
+                                intrepid travelers thronged here by the thousands to the trek in the lap of the mighty
+                                Himalaya’s, home to eight of the ten world’s highest mountain peaks. They came to lose
+                                themselves in the mystery of the fabled Shangri-La … and perhaps a cloud of ganja.</p>
+                            <p>
+
+                            </p>
+                            <p>As the years went by many people only saw Nepal as a place to trek or as an abode of the
+                                hippy, but as the twenty-first century dawned so did the age of adventure tourism. To
+                                many it became apparent that the Jaw Dropping Mountains of the Himalaya boasted so much
+                                more than trudging one foot in front of the other. Now, Nepal is perhaps the fastest
+                                growing adventure play ground in the South Asia and is firmly in the eyes of the world’s
+                                mountain biking community.</p>
+                        </div>
+                    </div>
+                    <div class="col-md-7" data-aos="fade-up">
+                        <div class="about-bimal">
+                            <div class="image-container">
+                                <img src="<?php echo bloginfo('template_directory') ?>/image/bimal-gurung.png"
+                                     alt="Bimal Gurung - Himalayan Dirt">
+                            </div>
+                            <div class="bimal-info">
+                                <div class="name-info">
+                                    <h2>Bimal <span>Gurung</span></h2>
+                                    <span>Founder/Lead Guide</span>
+                                </div>
+                                <div class="contact-info">
+                                    <div class="cn"><img
+                                                src="<?php echo bloginfo('template_directory') ?>/image/location.svg"
+                                                alt="phone"> <span>Sanepa - 2, Lalitpyr, Nepal</span></div>
+                                    <div class="cn"><img
+                                                src="<?php echo bloginfo('template_directory') ?>/image/phone.svg"
+                                                alt="phone"> <span>+977-9841234942</span></div>
+                                    <div class="cn"><img
+                                                src="<?php echo bloginfo('template_directory') ?>/image/email.svg"
+                                                alt="phone"> <span>info@himalayandirt.com</span></div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
             </section>
-            <section class="himalayan-intro-video">
-                <iframe src="https://www.youtube.com/embed/vfRlsBbD11c?autoplay=1&loop=1&controls=0&disablekb=1&mute=1"
-                        width="100%" style="height: 100vh" frameborder="0"></iframe>
+            <section class="himalayan-intro-video pt-0 pb-0" data-aos="flip-down">
+                <div class="intro-video-container">
+                    <iframe src="https://www.youtube.com/embed/vfRlsBbD11c?autoplay=1&loop=1&controls=0&disablekb=1&mute=1"
+                            width="100%" style="height: 100vh" frameborder="0"></iframe>
+                </div>
                 <div class="video-overlay"></div>
             </section>
-            <section id="himalayan-dirt-happy-riders">
+            <section id="himalayan-dirt-happy-riders" class="">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-8">
+                            <div class="section-title">
+                                <h2>Happy Riders</h2>
+                                <span>This is why we do what we do</span>
+                            </div>
                             <div class="owl-carousel owl-theme" id="happy-riders">
                                 <div class="riders-text">
 
