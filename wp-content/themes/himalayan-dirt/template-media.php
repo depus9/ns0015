269b9c2f1606f072ca92c01a53fb46e9be14b062
Krishna <krishnaabt@gmail.com>
2019-05-24 12:49:07 +0545

    add

---
+++ wp-content/themes/himalayan-dirt/template-media.php
@@ -0,0 +1,104 @@
+<?php
+//Template Name: Media
+get_header(); ?>
+    <div class="row">
+        <div id="primary" class="site-main-content" data-aos="zoom-in">
+            <header class="page-title sr-only">
+                <h1>Himalayan Dirt! with a rider owned Mountain Bike Tour Company</h1>
+            </header>
+            <div class="himalayan-hero-inner">
+                <div id="himalayan-hero-inner">
+                    <div class="hero-slider-item"
+                         style="background: url(<?php echo bloginfo('template_directory') ?>/image/himalayan-rides-bg.png);">
+                        <div class="container">
+                            <div class="hero-caption">
+                                <h2>Rides<span>Himalayan Dirt</span></h2>
+                                <span class="dark">With a rider owned mountain bike tour company.</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="arrow bounce">
+                    <img
+                            src="<?php echo bloginfo('template_directory'); ?>/image/arrow-white.svg" alt="back to top">
+                </div>
+</div> 
+        </div>
+    </div>
+    <div class="media-page-content" style="background-image:url(<?php echo bloginfo('template_directory');?>/image/media-page-bg.png);background-size:cover; background-position:center; min-height:100%;">
+    <div class="container">
+    <div class="row">
+	<div class="col-sm-12 col-md-4">	
+        <div class="card">
+				<div class="media-content" style="background-image:url(<?php echo bloginfo('template_directory');?>/image/we-are-hiring.png);background-size:cover; background-position:center; min-height:220px;">
+			</div>
+			<div class="media-caption" data-aos="slide-up">
+						<h3>We are hiring</h3>
+						<p>Do you love writing and creating content around mountain biking? Do you dream of 
+                        reporting on the latest developments in the sport and </p>
+                        <a href="#" title="Learn more about tilicho lake"><span>Learn More</span>
+                                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                         y="0px"
+                                         viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;"
+                                         xml:space="preserve">
+<path style="fill:#00aeef;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
+	C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
+	c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/>
+</svg>
+                    </a>
+					</div>
+                   
+				</div>
+    </div>
+    <div class="col-sm-12 col-md-4">	
+        <div class="card">
+				<div class="media-content" style="background-image:url(<?php echo bloginfo('template_directory');?>/image/media-thanked.png);background-size:cover; background-position:center; min-height:220px;">
+			</div>
+			<div class="media-caption" data-aos="slide-left">
+						<h3>Tianna Smith has thanked</h3>
+						<p>Tianna Smith has thanked the mountain bike community for its outpouring of emotion following the tragic death of her son, Steve Smith. </p>
+                        <a href="#" title="Learn more about tilicho lake"><span>Learn More</span>
+                                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                         y="0px"
+                                         viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;"
+                                         xml:space="preserve">
+<path style="fill:#00aeef;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
+	C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
+	c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/>
+</svg>
+                    </a>
+					</div>
+                   
+				</div>
+    </div>
+    
+    <div class="col-sm-12 col-md-4">	
+        <div class="card">
+				<div class="media-content" style="background-image:url(<?php echo bloginfo('template_directory');?>/image/media-travel.png);background-size:cover; background-position:center; min-height:220px;">
+			</div>
+			<div class="media-caption" data-aos="slide-right">
+						<h3>Travel boost for the Meta</h3>
+						<p>Do you love writing and creating content around mountain biking? Do you dream of reporting on the latest developments in the sport and  </p>
+                        <a href="#" title="Learn more about tilicho lake"><span>Learn More</span>
+                                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                         y="0px"
+                                         viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;"
+                                         xml:space="preserve">
+<path style="fill:#00aeef;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
+	C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
+	c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/>
+</svg>
+                    </a>
+					</div>
+                   
+				</div>
+	</div>
+
+    </div>
+    </div>
+    </div>
+    </div>
+<?php get_footer(); ?>
