ff133918b97bbc3155acffd61f042c7ca1059613
Deepesh Mali <deepeshmali@Deepeshs-MacBook-Air.local>
2019-05-24 13:13:29 +0545

    change on template files

--- wp-content/themes/himalayan-dirt/template-media.php
+++ wp-content/themes/himalayan-dirt/template-media.php
@@ -22,83 +22,132 @@ get_header(); ?>
                     <img
                             src="<?php echo bloginfo('template_directory'); ?>/image/arrow-white.svg" alt="back to top">
                 </div>
-</div> 
+            </div>
         </div>
     </div>
-    <div class="media-page-content" style="background-image:url(<?php echo bloginfo('template_directory');?>/image/media-page-bg.png);background-size:cover; background-position:center; min-height:100%;">
-    <div class="container">
-    <div class="row">
-	<div class="col-sm-12 col-md-4">	
-        <div class="card">
-				<div class="media-content" style="background-image:url(<?php echo bloginfo('template_directory');?>/image/we-are-hiring.png);background-size:cover; background-position:center; min-height:220px;">
-			</div>
-			<div class="media-caption" data-aos="slide-up">
-						<h3>We are hiring</h3>
-						<p>Do you love writing and creating content around mountain biking? Do you dream of 
-                        reporting on the latest developments in the sport and </p>
-                        <a href="#" title="Learn more about tilicho lake"><span>Learn More</span>
+    <div class="media-page-content"
+         style="background-image:url(<?php echo bloginfo('template_directory'); ?>/image/media-page-bg.png);background-size:cover; background-position:center; min-height:100%;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="ride-item blog" data-aos="slide-up" data-aos-delay="50">
+                        <div class="image-container">
+                            <img src="<?php echo bloginfo('template_directory') ?>/image/riders-1.png"
+                                 alt="Himalayan dirt ride to annapurna base camp">
+                        </div>
+                        <div class="text-content">
+                            <div class="himalayan-dirt-discription mt-0">
+                                <h2>Mesokanto la via Tilicho Lake</h2>
+                                <p>To provide all sorts of Mountain Biking Tours in the
+                                    Himalayas with the extensive knowledge of Terrains and Culture in a professional
+                                    manner.
+                                </p>
+                                <a href="#" title="Learn more about tilicho lake"><span>Learn More</span>
                                     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
                                          xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
                                          y="0px"
                                          viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;"
                                          xml:space="preserve">
-<path style="fill:#00aeef;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
+<path style="fill:#1E201D;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
 	C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
 	c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/>
 </svg>
-                    </a>
-					</div>
-                   
-				</div>
-    </div>
-    <div class="col-sm-12 col-md-4">	
-        <div class="card">
-				<div class="media-content" style="background-image:url(<?php echo bloginfo('template_directory');?>/image/media-thanked.png);background-size:cover; background-position:center; min-height:220px;">
-			</div>
-			<div class="media-caption" data-aos="slide-left">
-						<h3>Tianna Smith has thanked</h3>
-						<p>Tianna Smith has thanked the mountain bike community for its outpouring of emotion following the tragic death of her son, Steve Smith. </p>
-                        <a href="#" title="Learn more about tilicho lake"><span>Learn More</span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="ride-item blog" data-aos="slide-up" data-aos-delay="100">
+                        <div class="image-container">
+                            <img src="<?php echo bloginfo('template_directory') ?>/image/riders-1.png"
+                                 alt="Himalayan dirt ride to annapurna base camp">
+                        </div>
+                        <div class="text-content">
+                            <div class="himalayan-dirt-discription mt-0">
+                                <h2>Mesokanto la via Tilicho Lake</h2>
+                                <p>To provide all sorts of Mountain Biking Tours in the
+                                    Himalayas with the extensive knowledge of Terrains and Culture in a professional
+                                    manner.
+                                </p>
+                                <a href="#" title="Learn more about tilicho lake"><span>Learn More</span>
                                     <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
                                          xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
                                          y="0px"
                                          viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;"
                                          xml:space="preserve">
-<path style="fill:#00aeef;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
+<path style="fill:#1E201D;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
 	C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
 	c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/>
 </svg>
-                    </a>
-					</div>
-                   
-				</div>
-    </div>
-    
-    <div class="col-sm-12 col-md-4">	
-        <div class="card">
-				<div class="media-content" style="background-image:url(<?php echo bloginfo('template_directory');?>/image/media-travel.png);background-size:cover; background-position:center; min-height:220px;">
-			</div>
-			<div class="media-caption" data-aos="slide-right">
-						<h3>Travel boost for the Meta</h3>
-						<p>Do you love writing and creating content around mountain biking? Do you dream of reporting on the latest developments in the sport and  </p>
-                        <a href="#" title="Learn more about tilicho lake"><span>Learn More</span>
-                                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
-                                         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
-                                         y="0px"
-                                         viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;"
-                                         xml:space="preserve">
-<path style="fill:#00aeef;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
-	C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
-	c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/>
-</svg>
-                    </a>
-					</div>
-                   
-				</div>
-	</div>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
+                <div class="col-md-4">
+                    <div class="ride-item blog" data-aos="slide-up" data-aos-delay="150">
+                        <div class="image-container">
+                            <img src="<?php echo bloginfo('template_directory') ?>/image/riders-1.png"
+                                 alt="Himalayan dirt ride to annapurna base camp">
+                        </div>
+                        <div class="text-content">
+                            <div class="himalayan-dirt-discription mt-0">
+                                <h2>Mesokanto la via Tilicho Lake</h2>
+                                <p>To provide all sorts of Mountain Biking Tours in the
+                                    Himalayas with the extensive knowledge of Terrains and Culture in a professional
+                                    manner.
+                                </p>
+                                <a href="#" title="Learn more about tilicho lake"><span>Learn More</span>
+                                    <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                         xmlns:xlink="http://www.w3.org/1999/xlink" x="0px"
+                                         y="0px"
+                                         viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;"
+                                         xml:space="preserve">
+<path style="fill:#1E201D;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
+	C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
+	c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"/>
+</svg>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </div>
-    </div>
+    <div class="bottom-contact" data-aos="fade-in" data-aos-delay="150">
+        <div class="container">
+            <didv class="row">
+                <div class="col-md-7">
+                    <div class="contact-title">
+                        <h2>Want to learn more about Himalayan Dirt?</h2>
+                        <span>Let us know how er can get in touch with you.</span>
+                    </div>
+                </div>
+                <div class="col-md-4 offset-md-1">
+                    <form id="quickContact">
+                        <div class="form-group">
+                            <input type="text" class="form-control" placeholder="Name" required>
+                            <input type="email" class="form-control" placeholder="Email" required>
+                            <button type="submit" class="btn submit">Submit
+                                <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg"
+                                     xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                                     viewBox="0 0 31.49 31.49" style="enable-background:new 0 0 31.49 31.49;"
+                                     xml:space="preserve">
+                                    <path style="fill:#fff;" d="M21.205,5.007c-0.429-0.444-1.143-0.444-1.587,0c-0.429,0.429-0.429,1.143,0,1.571l8.047,8.047H1.111
+	C0.492,14.626,0,15.118,0,15.737c0,0.619,0.492,1.127,1.111,1.127h26.554l-8.047,8.032c-0.429,0.444-0.429,1.159,0,1.587
+	c0.444,0.444,1.159,0.444,1.587,0l9.952-9.952c0.444-0.429,0.444-1.143,0-1.571L21.205,5.007z"></path>
+                                    </svg>
+                            </button>
+                        </div>
+                    </form>
+
+                </div>
+            </didv>
+        </div>
     </div>
     </div>
 <?php get_footer(); ?>
