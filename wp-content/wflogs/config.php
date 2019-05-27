226ae640d4c76e1bdf3bdd88b8bc42b9f4e341b1
Deepesh Mali <deepeshmali@Deepeshs-MacBook-Air.local>
2019-05-27 14:29:07 +0545

    changes

--- wp-content/wflogs/config.php
+++
@@ -1,6 +0,0 @@
-<?php exit('Access denied'); __halt_compiler(); ?>
-******************************************************************
-This file is used by the Wordfence Web Application Firewall. Read 
-more at https://docs.wordfence.com/en/Web_Application_Firewall_FAQ
-******************************************************************
-a:7:{s:9:"wafStatus";s:13:"learning-mode";s:30:"learningModeGracePeriodEnabled";i:1;s:23:"learningModeGracePeriod";i:1559542152;s:7:"authKey";s:64:"z[g)N7|o!;ig-,(N@h5U>!vQ`llqzW$[^}2OK$V;!rfcI!|Dg.^%~g:{0o&5:qbc";s:7:"version";s:5:"1.0.4";s:11:"wafDisabled";b:0;s:13:"attackDataKey";i:991;}
