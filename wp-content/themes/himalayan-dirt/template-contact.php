059830c110a6f312cc9f26d3f004a67de9da04a6
Deepesh Mali <deepeshmali@Deepeshs-MacBook-Air.local>
2019-05-28 14:56:09 +0545

    about section contact updates

--- wp-content/themes/himalayan-dirt/template-contact.php
+++ wp-content/themes/himalayan-dirt/template-contact.php
@@ -28,9 +28,9 @@ get_header(); ?>
         <div class="container">
             <div class="row">
                 <div class="col-ms-12 col-md-7">
-                   <?php
-                   echo do_shortcode('[contact-form-7 id="20" title="Main contact form"]');
-                   ?>
+                    <?php
+                    echo do_shortcode('[contact-form-7 id="20" title="Main contact form"]');
+                    ?>
                 </div>
                 <div class="col-ms-12 col-md-5">
                     <div class="himalayan-contact himalaya-address">
